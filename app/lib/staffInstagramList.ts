48c3ad92aad2dfa1e92a5569f376e78179f20535
nari <yasunarj@gmail.com>
2025-04-16 20:24:25 +0900

    insta

--- app/lib/staffInstagramList.ts
+++ app/lib/staffInstagramList.ts
@@ -1,6 +1,6 @@
 export const staffInstagramList = [
   { name: "あつし", handle: 'atushi' },
   { name: "熱波送郎", handle: 'neppasourou'},
-  { name: "熱虎", handle: 'netutora' },
+  { name: "熱虎", handle: 'nettora.neppashi/' },
   { name: "フェレット伊藤", handle: 'ferret_ito' },
 ];
