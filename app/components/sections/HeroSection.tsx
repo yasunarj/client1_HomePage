9667fb7be8d3695962e37a0c4d09b53facf48282
nari <yasunarj@gmail.com>
2025-04-17 16:25:23 +0900

    fiexs: heroImage

--- app/components/sections/HeroSection.tsx
+++ app/components/sections/HeroSection.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function HeroSection() {
   return (
-    <section className="relative h-screen flex items-center justify-center overflow-hidden ">
+    <section className="relative min-h-screen flex items-center justify-center bg-black">
       <Image
         src="/images/gallery1.jpg"
         alt="サウナの熱波"
