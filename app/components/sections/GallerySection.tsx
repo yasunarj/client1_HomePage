58496d926b394d8b73d362629f86fe9a6c5b1e7c
nari <yasunarj@gmail.com>
2025-04-17 16:13:52 +0900

    fiexs: gallery

--- app/components/sections/GallerySection.tsx
+++ app/components/sections/GallerySection.tsx
@@ -19,6 +19,7 @@ export default function GallerySection() {
                   fill
                   className="object-cover"
                   alt="ギャラリー画像"
+                  loading="eager"
                 />
               </div>
             ))}
