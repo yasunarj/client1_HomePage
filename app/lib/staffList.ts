20fa980d3de0c62f9a1252aa707b7e69e1529f11
nari <yasunarj@gmail.com>
2025-04-18 17:49:14 +0900

    fiexs: profile

--- app/lib/staffList.ts
+++ app/lib/staffList.ts
@@ -1,15 +1,16 @@
 export const staffList = [
   {
-    name: "あっしー",
+    name: "熱波送郎",
     role: "代表熱波師",
-    src: "/images/IMG_2954.png",
-    description: "神奈川県の激戦区で認められた熱波師",
+    src: "/images/IMG_2959.png",
+    description:
+      "フィンランドでの研修経験あり。ダンディーな見た目ながら繊細な熱波を提供。",
   },
   {
-    name: "熱波送郎",
+    name: "あっしー",
     role: "熱波師",
-    src: "/images/IMG_2959.png",
-    description: "フィンランドでの研修経験あり。ダンディーな見た目ながら繊細な熱波を提供。",
+    src: "/images/IMG_2954.png",
+    description: "神奈川県の激戦区で認められた熱波師",
   },
   {
     name: "熱虎",
