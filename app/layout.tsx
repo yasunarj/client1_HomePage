ccf805f81b899bc78dcc8442713d04a1282d2db8
nari <yasunarj@gmail.com>
2025-05-16 19:06:16 +0900

    モバイル画面の拡大修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "大田原熱波協会",
   description: "プロフェッショナルな熱波師があなたのサウナ体験を演出します",
-  // viewport: "width=device-width, initial-scale=1",
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1",
 };
 
 export default function RootLayout({
