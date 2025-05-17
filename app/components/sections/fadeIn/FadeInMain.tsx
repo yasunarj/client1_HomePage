9384dcba5f1de67723a997a6e70997ff87cddc90
nari <yasunarj@gmail.com>
2025-05-18 01:02:36 +0900

    フェードイン時間修正

--- app/components/sections/fadeIn/FadeInMain.tsx
+++ app/components/sections/fadeIn/FadeInMain.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from "react";
 
 const FadeInMain = ({ children }: { children: ReactNode }) => {
   return (
-    <motion.main initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 2, ease: "easeOut" }}>
+    <motion.main initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 3 }}>
       { children }
     </motion.main>
   );
