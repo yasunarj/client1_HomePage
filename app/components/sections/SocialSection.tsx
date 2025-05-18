99f0dbc0f1178fe3094c76fed698f2255e50a0f4
nari <yasunarj@gmail.com>
2025-05-18 19:51:12 +0900

    scroll調整

--- app/components/sections/SocialSection.tsx
+++ app/components/sections/SocialSection.tsx
@@ -31,7 +31,7 @@ export default function SocialSection() {
         </h2>
         <div className="relative grid grid-cols-1 md:grid-cols-2 gap-8 z-10">
           {/* 会社用Instagram */}
-          <div className="w-full max-w-lg mx-auto">
+          <div className="w-full max-w-md sm:max-w-lg mx-auto">
             <div className="flex flex-col bg-white rounded-xl overflow-hidden shadow-lg max-h-[720px] sm:min-h-[720px]">
               <div className="bg-gradient-to-r from-orange-500 to-red-600">
                 <h3 className="text-xl font-bold text-white text-center">
@@ -45,7 +45,7 @@ export default function SocialSection() {
           </div>
 
           {/* 個人用Instagram */}
-          <div className="w-full max-w-lg mx-auto h-full min-h-[500px]">
+          <div className="w-full max-w-md sm:max-w-lg mx-auto h-full min-h-[500px]">
             <div className="flex flex-col bg-white rounded-xl shadow-lg overflow-hidden max-h-[720px]">
               <div className="bg-gradient-to-r from-orange-500 to-red-600">
                 <h3 className="text-xl font-bold text-white text-center">
