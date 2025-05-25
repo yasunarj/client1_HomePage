8a7bb360cbbf4068d408dc0f6fb7e108877476a0
nari <yasunarj@gmail.com>
2025-05-25 15:40:49 +0900

    Hero画像の表示時間

--- app/components/sections/HeroSection.tsx
+++ app/components/sections/HeroSection.tsx
@@ -1,6 +1,11 @@
-import Image from 'next/image';
+"use client";
+
+import { motion } from "framer-motion";
+import Image from "next/image";
+import { useState } from "react";
 
 export default function HeroSection() {
+  const [isImageLoaded, setIsImageLoaded] = useState<boolean>(false);
   return (
     <section className="relative h-[100dvh] flex items-center justify-center">
       <Image
@@ -11,16 +16,22 @@ export default function HeroSection() {
         loading="eager"
         priority
         quality={80}
+        onLoadingComplete={() => setIsImageLoaded(true)}
       />
       <div className="absolute inset-0 bg-black/20"></div>
-      <div className={`relative z-10 text-center text-white px-4`}>
+      <motion.div
+        className={`relative z-10 text-center text-white px-4`}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: isImageLoaded ? 1 : 0 }}
+        transition={{ duration: 1 }}
+      >
         <h1 className="text-5xl md:text-7xl font-bold mb-6 animate-fadeInUp ">
           熱波の芸術
         </h1>
         <p className="text-xl md:text-2xl animate-fadeInUp delay-300">
           心と体を癒す、究極のサウナ体験
         </p>
-      </div>
+      </motion.div>
     </section>
   );
-} 
+}
