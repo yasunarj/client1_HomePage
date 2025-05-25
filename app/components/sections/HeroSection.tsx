953863b57e52852f13de075342b481be2b436ead
nari <yasunarj@gmail.com>
2025-05-25 15:52:02 +0900

    Hero画像の表示時間

--- app/components/sections/HeroSection.tsx
+++ app/components/sections/HeroSection.tsx
@@ -7,7 +7,12 @@ import { useState } from "react";
 export default function HeroSection() {
   const [isImageLoaded, setIsImageLoaded] = useState<boolean>(false);
   return (
-    <section className="relative h-[100dvh] flex items-center justify-center bg-white">
+    <motion.div
+      className="relative h-[100dvh] flex items-center justify-center bg-white"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: isImageLoaded ? 1 : 0 }}
+      transition={{ duration: 2 }}
+    >
       <Image
         src="/images/gallery1.jpg"
         alt="熱波師がサウナ室で風を送る様子（栃木サウナ熱波協会）"
@@ -19,19 +24,14 @@ export default function HeroSection() {
         onLoadingComplete={() => setIsImageLoaded(true)}
       />
       <div className="absolute inset-0 bg-black/20"></div>
-      <motion.div
-        className={`relative z-10 text-center text-white px-4`}
-        initial={{ opacity: 0 }}
-        animate={{ opacity: isImageLoaded ? 1 : 0 }}
-        transition={{ duration: 2 }}
-      >
+      <div className={`relative z-10 text-center text-white px-4`}>
         <h1 className="text-5xl md:text-7xl font-bold mb-6 animate-fadeInUp ">
           熱波の芸術
         </h1>
         <p className="text-xl md:text-2xl animate-fadeInUp delay-300">
           心と体を癒す、究極のサウナ体験
         </p>
-      </motion.div>
-    </section>
+      </div>
+    </motion.div>
   );
 }
