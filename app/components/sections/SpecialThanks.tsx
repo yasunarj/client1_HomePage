da4784a8fcfc1ec543b15e24be84d1b29df94856
nari <yasunarj@gmail.com>
2025-09-12 18:44:26 +0900

    fixed: SpecialThanks.tsx

--- app/components/sections/SpecialThanks.tsx
+++ app/components/sections/SpecialThanks.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import Container from "../layout/Container";
 import { useFadeInOnView } from "@/hooks/useFadeInOnView";
 import PartnerCard from "./PartnerCard";
 import { partners } from "@/app/lib/partners";
@@ -10,25 +9,27 @@ const SpecialThanks = () => {
 
   return (
     <section className="py-20 bg-gray-50">
-      <Container>
-        <h2
-          ref={title.ref}
-          className={`text-3xl sm:text-4xl font-bold text-center mb-16 reveal-text ${
-            title.isVisible ? "revealed" : ""
-          }`}
-        >
-          <span>パートナー温泉施設</span>
-        </h2>
+      <div className={`w-full max-w-[100vw] overflow-x-hidden px-4`}>
+        <div className="max-w-8xl mx-auto">
+          <h2
+            ref={title.ref}
+            className={`text-3xl sm:text-4xl font-bold text-center mb-16 reveal-text ${
+              title.isVisible ? "revealed" : ""
+            }`}
+          >
+            <span>パートナー温泉施設</span>
+          </h2>
 
-        <div
-          className={`grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 items-stretch
+          <div
+            className={`grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 max-w-[1920px] mx-auto mb-4
         `}
-        >
-          {partners.map((p) => (
-            <PartnerCard key={p.id} partner={p} />
-          ))}
+          >
+            {partners.map((p) => (
+              <PartnerCard key={p.id} partner={p} />
+            ))}
+          </div>
         </div>
-      </Container>
+      </div>
     </section>
   );
 };
