a9523bbcc167ad0afd5bd289c07e1c90a78e5938
Takahashi Yasunari <yasunarj@gmail.com>
2026-06-19 16:23:20 +0900

    add: ScheculeTitle_Additional_Notes

--- app/components/sections/ScheduleTitle.tsx
+++ app/components/sections/ScheduleTitle.tsx
@@ -4,19 +4,26 @@ import { useFadeInOnView } from "@/hooks/useFadeInOnView";
 const ScheduleTitle = () => {
   const title = useFadeInOnView();
   return (
-    <h2
-      ref={title.ref}
-      className={`text-3xl sm:text-4xl font-bold text-center mb-16
+    <div ref={title.ref} className="text-center mb-16">
+      <h2
+        className={`text-3xl sm:text-4xl font-bold
       }`}
-    >
-      <span
-        className={`transition-color duration-[2s] ${
-          title.isVisible ? "text-white" : "text-orange-600"
-        }`}
       >
-        イベントスケジュール
-      </span>
-    </h2>
+        <span
+          className={`transition-color duration-[2s] ${
+            title.isVisible ? "text-white" : "text-orange-600"
+          }`}
+        >
+          イベントスケジュール
+        </span>
+      </h2>
+
+      <p
+        className={`mt-2 text-sm sm:text-base transition-colors duration-[2s] ${title.isVisible ? "text-white/90" : "text-gray-600"}`}
+      >
+        ※ 急遽予定が変わる場合もございます
+      </p>
+    </div>
   );
 };
 
